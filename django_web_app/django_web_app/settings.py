1a976d5f678ff55634b76a3e1e0b2c94735ab09c
Mahesh Sawant <48624870+smahesh29@users.noreply.github.com>
2019-03-31 19:45:51 +0530

    Update settings.py

--- django_web_app/django_web_app/settings.py
+++ django_web_app/django_web_app/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = '@5&-q%^o=@mb@=@e%b9yz^b#l-2)w&_s0ick#=wy3kw36$z($g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
