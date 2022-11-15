f76526eca2e728c2fe4e5294f2034661dbec0339
lyjnamur <92183339+lyjnamur@users.noreply.github.com>
2022-11-15 16:34:53 +0100

    update: add captcha

--- django_web_app/users/forms.py
+++ django_web_app/users/forms.py
@@ -2,11 +2,11 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from .models import Profile
-
+from captcha.fields import CaptchaField
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
-
+    captcha = CaptchaField()
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
@@ -14,7 +14,7 @@ class UserRegisterForm(UserCreationForm):
 
 class UserUpdateForm(forms.ModelForm):
     email = forms.EmailField()
-
+    captcha = CaptchaField()
     class Meta:
         model = User
         fields = ['username', 'email']
