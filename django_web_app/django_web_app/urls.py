43536b16047732f5515c0f82dcbdaddd8627414d
lyjnamur <92183339+lyjnamur@users.noreply.github.com>
2022-11-15 16:31:31 +0100

    update: add captcha

--- django_web_app/django_web_app/urls.py
+++ django_web_app/django_web_app/urls.py
@@ -27,6 +27,7 @@ urlpatterns = [
     path('profile/', user_views.profile, name='profile'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('', include('blog.urls')),
+    path('captcha/', include('captcha.urls')),
 ]
 
 
