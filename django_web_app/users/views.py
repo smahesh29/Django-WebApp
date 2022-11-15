30945e55347cac6b39e77ec27aa1385f054c4dfa
lyjnamur <92183339+lyjnamur@users.noreply.github.com>
2022-11-15 16:34:29 +0100

    update: add captcha

--- django_web_app/users/views.py
+++ django_web_app/users/views.py
@@ -24,10 +24,12 @@ def profile(request):
         p_form = ProfileUpdateForm(request.POST,
                                    request.FILES,
                                    instance=request.user.profile)
-        if u_form.is_valid() and p_form.is_valid():
+        if (u_form.is_valid() and p_form.is_valid()):
             u_form.save()
             p_form.save()
             messages.success(request, f'Your account has been updated!')
+            return redirect('profile')
+        else:
             return redirect('profile')
 
     else:
