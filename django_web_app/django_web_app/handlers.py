3c4f809d2361e052c016f5b96b96e79b4f6efacb
lyjnamur <92183339+lyjnamur@users.noreply.github.com>
2022-11-29 19:45:46 +0100

    Delete handlers.py

--- django_web_app/django_web_app/handlers.py
+++
@@ -1,8 +0,0 @@
-from corsheaders.signals import check_request_enabled
-
-
-def cors_allow_api_to_everyone(sender, request, **kwargs):
-    return request.path.startswith("/api/")
-
-
-check_request_enabled.connect(cors_allow_api_to_everyone)
