79b365a6e324fc0c82c58ddd861529934cc8a447
sraonekumar <msraonekumar@gmail.com>
2023-06-01 20:27:08 +0530

    Dockerized the app and resolved /register, /login issues

--- django_web_app/django_web_app/settings.py
+++ django_web_app/django_web_app/settings.py
@@ -15,7 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
